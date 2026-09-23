3646e59f9f37fdca4698ca2b033ec270cc6f67a6
sdobinancity2026-eng <sdobinancity2026@gmail.com>
2026-09-23 03:57:54 +0000

    commit

--- .next/dev/static/chunks/src_0oh2th7._.js
+++ .next/dev/static/chunks/src_0oh2th7._.js
@@ -1231,7 +1231,7 @@ __turbopack_context__.s([
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$build$2f$polyfills$2f$process$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = /*#__PURE__*/ __turbopack_context__.i("[project]/node_modules/next/dist/build/polyfills/process.js [app-client] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f40$supabase$2f$supabase$2d$js$2f$dist$2f$index$2e$mjs__$5b$app$2d$client$5d$__$28$ecmascript$29$__$3c$locals$3e$__ = __turbopack_context__.i("[project]/node_modules/@supabase/supabase-js/dist/index.mjs [app-client] (ecmascript) <locals>");
 ;
-const supabaseUrl = ("TURBOPACK compile-time value", "https://your-actual-project-ref.supabase.co") || '';
+const supabaseUrl = ("TURBOPACK compile-time value", "https://hbdeyxlddttygggmffsj.supabase.co") || '';
 const supabaseAnonKey = ("TURBOPACK compile-time value", "sb_publishable_qiJWVSK8sHevVBb10z54OA_FPJ91MDe") || '';
 const supabase = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f40$supabase$2f$supabase$2d$js$2f$dist$2f$index$2e$mjs__$5b$app$2d$client$5d$__$28$ecmascript$29$__$3c$locals$3e$__["createClient"])(supabaseUrl, supabaseAnonKey);
 if (typeof globalThis.$RefreshHelpers$ === 'object' && globalThis.$RefreshHelpers !== null) {
